96901de5518c7984675a218b5fc55b557f3e794c
wangyang <wangyangg@aliyun.com>
2023-03-09 16:17:35 +0800

    feat:添加配置文件

---
+++ springboot/springboot-01-hello/src/main/java/com/yang/springboot/config/MyConfig.java
@@ -0,0 +1,14 @@
+package com.yang.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: yang
+ * @date: 2023/3/9 16:14
+ * @desc:
+ */
+@Configuration
+public class MyConfig {
+
+
+}
