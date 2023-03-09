dc71f78c9347733856cb8e182457b79648289edf
wangyang <wangyangg@aliyun.com>
2023-03-09 18:24:54 +0800

    feat:@Configuration @Import @ConditionalOnBean使用

--- springboot/springboot-01-hello/src/main/java/com/yang/springboot/MainApplication.java
+++ springboot/springboot-01-hello/src/main/java/com/yang/springboot/MainApplication.java
@@ -1,5 +1,6 @@
 package com.yang.springboot;
 
+import com.yang.springboot.entity.Animal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -13,11 +14,21 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class MainApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
-        boolean user = run.containsBean("user");
-        System.out.println(user);
-        System.out.println("============");
-        for (String beanDefinitionName : run.getBeanDefinitionNames()) {
+        String[] beanDefinitionNames = run.getBeanDefinitionNames();
+        //com.yang.springboot.entity.Animal
+        for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
         }
+        boolean user = run.containsBean("user");
+
+        boolean botany = run.containsBean("botany");
+        System.out.println("存在植物："+ botany);
+        System.out.println("存在人类："+ user);
+
+//        System.out.println(user);
+//        System.out.println("============");
+//        for (String beanDefinitionName : run.getBeanDefinitionNames()) {
+//            System.out.println(beanDefinitionName);
+//        }
     }
 }
--- springboot/springboot-01-hello/src/main/java/com/yang/springboot/config/MyConfig.java
+++ springboot/springboot-01-hello/src/main/java/com/yang/springboot/config/MyConfig.java
@@ -1,6 +1,12 @@
 package com.yang.springboot.config;
 
+import com.yang.springboot.entity.Animal;
+import com.yang.springboot.entity.Botany;
+import com.yang.springboot.entity.User;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author: yang
@@ -8,7 +14,24 @@ import org.springframework.context.annotation.Configuration;
  * @desc:
  */
 @Configuration
+@Import(Animal.class)
+//import 引入的组件默认id是全类名 com.yang.springboot.entity.Animal
 public class MyConfig {
+
+    @Bean
+    public Botany botany(){
+        return new Botany();
+    }
+
+
+    @ConditionalOnBean(Botany.class)
+    @Bean
+    public User user(){
+        return new User();
+    }
+
+
+
 
 
 }
